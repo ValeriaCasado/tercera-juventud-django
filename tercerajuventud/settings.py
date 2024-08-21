27ef7fe1033c2f15b421906bb612521841d51f95
valeria <valeria@valerias-MacBook-Pro.local>
2024-08-21 16:11:48 +0200

    Dockerfile and settings config

--- tercerajuventud/settings.py
+++ tercerajuventud/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('os.getenv')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', False)
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True' if os.getenv('DJANGO_DEBUG') else False
 
 ALLOWED_HOSTS = ['*']
 
